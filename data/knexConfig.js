77baa7f7f9df164c6672f3b29ef1abf61918c74d
Angel Torres <alas4224@gmail.com>
2019-03-08 20:43:44 -0600

    add migrations

--- data/knexConfig.js
+++ data/knexConfig.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const knexConfig = require('../knexfile');
 const knex = require('knex');
 
-const dbEnv = process.env.DB_ENV || 'testing';
+const dbEnv = process.env.DB_ENV || 'development';
 
 module.exports = knex(knexConfig[dbEnv]);
